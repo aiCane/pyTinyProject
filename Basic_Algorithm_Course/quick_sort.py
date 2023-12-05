c6555654e23938d6f053d1b7c4ba981255d3230b
harry-yilin-zhang <harry.yilin.zhang@outlook.com>
2023-12-05 09:15:02 +0800

    template 1.0

--- Basic_Algorithm_Course/quick_sort.py
+++ Basic_Algorithm_Course/quick_sort.py
@@ -1,23 +1,21 @@
-def quick_sort(q, l, r):
-    if l >= r:
+def quick_sort(q, ll, r):
+    if ll >= r:
         return q
-    x = q[l]
-    i = l - 1
+    x = q[ll]
+    i = ll - 1
     j = r + 1
-    # i = l
-    # j = r
     while i < j:
         while True:  # do i++; while (q[i] < x);
             i += 1
-            if q[i] > x:
+            if q[i] >= x:  # if not q[i] < x:
                 break
         while True:
             j -= 1
-            if q[j] < x:
+            if q[j] <= x:
                 break
         if i < j:
             q[i], q[j] = q[j], q[i]
-    quick_sort(q, l, j)
+    quick_sort(q, ll, j)
     quick_sort(q, j + 1, r)
 
 
@@ -25,5 +23,4 @@ if __name__ == "__main__":
     n = int(input("n: "))
     lst = [int(num) for num in input("numbers: ").split()]
     quick_sort(lst, 0, n - 1)
-    print()
-    print(lst)
+    [print(i, end=" ") for i in lst]
